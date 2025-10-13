8a92dcfa904198d2dd46eb4394f2a6ace24fbbe1
zizicici <1940699+zizicici@users.noreply.github.com>
2025-10-13 23:26:59 +0800

    [feat] New Project

---
+++ Pin It/MainViewController.swift
@@ -0,0 +1,21 @@
+//
+//  MainViewController.swift
+//  Pin It
+//
+//  Created by Ci Zi on 2025/10/13.
+//
+
+import UIKit
+
+class MainViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+        
+        view.backgroundColor = UIColor.background
+    }
+
+
+}
+
