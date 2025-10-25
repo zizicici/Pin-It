87f54aaebe8f048c5d9b18cc17f0e172ec0d3471
zizicici <1940699+zizicici@users.noreply.github.com>
2025-10-25 22:31:45 +0800

    [feat] AddPostIntent

---
+++ Pin It/AppIntents/AddPostIntent.swift
@@ -0,0 +1,27 @@
+//
+//  AddPostIntent.swift
+//  Pin It
+//
+//  Created by Ci Zi on 2025/10/25.
+//
+
+import AppIntents
+
+struct AddTextRecordIntent: LiveActivityIntent {
+    static var title: LocalizedStringResource = "intent.post.add.by.text.title"
+    
+    static var description: IntentDescription = IntentDescription("intent.post.add.by.text.description", categoryName: "intent.post.add.category")
+    
+    @Parameter(title: "intent.text")
+    var content: String
+    
+    static var parameterSummary: some ParameterSummary {
+        Summary("intent.post.add.by.text.summary\(\.$content)")
+    }
+    
+    @MainActor
+    func perform() async throws -> some IntentResult & ReturnsValue<Bool> {
+        let result = DataManager.shared.createPost(content: content)
+        return .result(value: result)
+    }
+}
