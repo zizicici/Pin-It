b40a46ccd15c82eb790fa9692b723e42b139132d
zizicici <1940699+zizicici@users.noreply.github.com>
2025-11-21 15:16:10 +0800

    [chore] Bump Version: 1.0.8 -> 1.0.9

--- Pin It/More/SpecificationsViewController.swift
+++ Pin It/More/SpecificationsViewController.swift
@@ -73,7 +73,7 @@ class SpecificationsViewController: UIViewController {
                 case .publisher:
                     return "ZIZICICI LIMITED"
                 case .date:
-                    return "2025/11/19"
+                    return "2025/11/21"
                 case .license:
                     return "粤ICP备2025448771号-4A"
                 }
